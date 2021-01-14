360eb5b09f6240128453b7d654f10ae2c9b68bca
Sylvie-B <unsolo@hotmail.fr>
2021-01-14 10:12:44 +0100

    script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+
+// console.log(player);
